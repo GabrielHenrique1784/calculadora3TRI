1faa3aa10012856497617f2bc6bf337f0cfd7e24
gabrielhenrique1784 <gabriel.h.pereira2008@gmail.com>
2023-12-08 15:35:13 -0300

    KEYPRESS

--- script.js
+++ script.js
@@ -34,4 +34,63 @@ function calcular() {
     }
 }
 
+//KEYPRESS=================================
 
+
+document.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        calcular();
+    }
+    if (event.key == "1") {
+        inserir(1);
+    }
+    if (event.key == "2") {
+        inserir(2);
+    }
+    if (event.key == "3") {
+        inserir(3);
+    }
+    if (event.key == "4") {
+        inserir(4);
+    }
+    if (event.key == "5") {
+        inserir(5);
+    }
+    if (event.key == "6") {
+        inserir(6);
+    }
+    if (event.key == "7") {
+        inserir(7);
+    }
+    if (event.key == "8") {
+        inserir(8);
+    }
+    if (event.key == "9") {
+        inserir(9);
+    }
+    if (event.key == "0") {
+        inserir(0);
+    }
+    if (event.key == "+") {
+        inserir("+");
+    }
+    if (event.key == "-") {
+        inserir("-");
+    }
+    if (event.key == "*") {
+        inserir("*");
+    }
+    if (event.key == "/") {
+        inserir("/");
+    }
+    if (event.key == "Delete") {
+        voltar();
+    }
+    if (event.key == "c") {
+        limpar()
+    }
+    if (event.key == ".") {
+
+        inserir(".");
+    }
+})
